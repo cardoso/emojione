032534ecbafb21bb3021a82fac491e9cc5501e12
cardoso <matheus@cardo.so>
2017-12-21 19:25:32 -0300

    Update swift template

--- lib/swift/generator/Emojione.swift
+++ lib/swift/generator/Emojione.swift
@@ -19,19 +19,19 @@ struct Emojione {
     static func transform(string: String) -> String {
         let oldString = string as NSString
         var transformedString = string as NSString
-        
-        let regex = try! NSRegularExpression(pattern: ":([-+\\w]+):", options: [])
-        let matches = regex.matches(in: transformedString as String, options: [], range: NSMakeRange(0, transformedString.length))
-        
+
+        let regex = try? NSRegularExpression(pattern: ":([-+\\w]+):", options: [])
+        let matches = regex?.matches(in: transformedString as String, options: [], range: NSRange(location: 0, length: transformedString.length)) ?? []
+
         for result in matches {
             guard result.numberOfRanges == 2 else { continue }
-            
-            let shortname = oldString.substring(with: result.rangeAt(1))
+
+            let shortname = oldString.substring(with: result.range(at: 1))
             if let emoji = values[shortname] {
                 transformedString = transformedString.replacingOccurrences(of: ":\(shortname):", with: emoji) as NSString
             }
         }
-        
+
         return transformedString as String
     }
 }
