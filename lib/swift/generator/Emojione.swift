e95e7e7f0590a8805e6c872f5dcd967e4c5ff77c
cardoso <matheus@cardo.so>
2017-12-27 08:50:51 -0300

    Add name and keywords to emojis

--- lib/swift/generator/Emojione.swift
+++ lib/swift/generator/Emojione.swift
@@ -7,14 +7,30 @@
 
 import Foundation
 
+struct Emoji {
+    let name: String
+    let shortname: String
+    let supportsTones: Bool
+    let alternates: [String]
+    let keywords: [String]
+
+    init(_ name: String, _ shortname: String, _ supportsTones: Bool, _ alternates: [String], _ keywords: [String]) {
+        self.name = name
+        self.shortname = shortname
+        self.supportsTones = supportsTones
+        self.alternates = alternates
+        self.keywords = keywords
+    }
+}
+
+// swiftlint:disable type_body_length
+// swiftlint:disable file_length
 struct Emojione {
     static let values = [
         <%= mapping %>
     ]
 
-    static let categories: [String: [(shortname: String, supportsTones: Bool)]] = [
-        <%= categories %>
-    ]
+    <%= categories %>
     
     static func transform(string: String) -> String {
         let oldString = string as NSString
